e5f94e36c600069bea5fe866c6fdfbaf610af88e
Nicolas Oyague <94309353+nico92-pe@users.noreply.github.com>
2025-08-19 13:46:47 -0400

    Updated ProductForm.tsx

--- src/pages/products/ProductForm.tsx
+++ src/pages/products/ProductForm.tsx
@@ -307,8 +307,6 @@ export function ProductForm() {
                     Precio Minorista *
                   </label>
                   <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-muted-foreground">
-                    </div>
                     <input
                       id="retailPrice"
                       type="number"
