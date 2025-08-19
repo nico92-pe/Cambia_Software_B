e71406270ad1dabfbcbefa576dc60bcf2042edcc
Nicolas Oyague <94309353+nico92-pe@users.noreply.github.com>
2025-08-19 19:18:29 -0400

    Updated ProductForm.tsx

--- src/pages/products/ProductForm.tsx
+++ src/pages/products/ProductForm.tsx
@@ -278,7 +278,6 @@ export function ProductForm() {
                   </label>
                   <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-muted-foreground">
-                      <DollarSign size={18} />
                     </div>
                     <input
                       id="wholesalePrice"
