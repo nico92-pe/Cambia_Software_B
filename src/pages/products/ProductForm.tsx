4e15875c504b5f4ffd9a40512a6e997236829f35
Nicolas Oyague <94309353+nico92-pe@users.noreply.github.com>
2025-08-19 20:29:40 -0400

    Updated ProductForm.tsx

--- src/pages/products/ProductForm.tsx
+++ src/pages/products/ProductForm.tsx
@@ -278,8 +278,7 @@ export function ProductForm() {
                     Precio Mayorista *
                   </label>
                   <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-muted-foreground">
-                    </div>
+
                     <input
                       id="wholesalePrice"
                       type="number"
